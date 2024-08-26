67e517b2cd54fa10bce0499138109adff28793d4
Carsten Gräf <evandor@gmail.com>
2024-08-26 08:53:20 +0200

    wip

--- persistence/FirestoreThumbnailsPersistence.ts
+++ persistence/FirestoreThumbnailsPersistence.ts
@@ -1,7 +1,8 @@
 import ThumbnailsPersistence from "src/thumbnails/persistence/ThumbnailsPersistence";
-import {getBytes, ref, uploadString, deleteObject} from "firebase/storage";
+import {getBytes, ref, uploadString, deleteObject, listAll, getMetadata} from "firebase/storage";
 import FirebaseServices from "src/services/firebase/FirebaseServices";
 import {useAuthStore} from "stores/authStore";
+import {doc, updateDoc} from "firebase/firestore";
 
 class FirestoreThumbnailsPersistence extends ThumbnailsPersistence {
 
@@ -13,6 +14,7 @@ class FirestoreThumbnailsPersistence extends ThumbnailsPersistence {
   saveThumbnail(tabId: string, thumbnail: string): Promise<void> {
     console.log(`saving Thumbnail ${tabId}`)
     this.saveBlobToStorage(tabId, thumbnail);
+    this.updateStorageQuote()
     return Promise.resolve()
   }
 
@@ -23,13 +25,14 @@ class FirestoreThumbnailsPersistence extends ThumbnailsPersistence {
     return decoder.decode(res)
   }
 
-  cleanUpThumbnails(fnc: (url: string) => boolean): Promise<void> {
-    return Promise.reject("cleanUpThumbnails not implemented in FirestoreThumbnailsPersistence");
-  }
+  // cleanUpThumbnails(fnc: (url: string) => boolean): Promise<void> {
+  //   return Promise.reject("cleanUpThumbnails not implemented in FirestoreThumbnailsPersistence");
+  // }
 
   async deleteThumbnail(tabId: string): Promise<void> {
     const storageReference = ref(FirebaseServices.getStorage(), `users/${useAuthStore().user.uid}/thumbnails/${tabId}`);
     await deleteObject(storageReference)
+    await this.updateStorageQuote()
     return Promise.resolve()
   }
 
@@ -41,6 +44,28 @@ class FirestoreThumbnailsPersistence extends ThumbnailsPersistence {
     return tabId;
   }
 
+  private async saveQuote(quote: object) {
+    if (useAuthStore().user?.uid) {
+      const userDoc = doc(FirebaseServices.getFirestore(), "users", useAuthStore().user?.uid)
+      await updateDoc(userDoc, quote)
+    }
+  }
+
+  private async updateStorageQuote() {
+    const userThumnnails = ref(FirebaseServices.getStorage(), `users/${useAuthStore().user.uid}/thumbnails`);
+    const res = await listAll(userThumnnails)
+    let size = 0
+    for (const itemRef of res.items) {
+      try {
+        const md = await getMetadata(itemRef)
+        size += md.size
+      } catch (err) {
+        // ignore, document might have been deleted
+      }
+    }
+    //console.log("size", size)
+    await this.saveQuote({thumbnails: Math.round(100 * size / (1024*1024)) / 100})
+  }
 
 }
 
